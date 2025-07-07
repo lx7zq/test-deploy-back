637d889adb2780bf8f6f97d6db2afc59e5b85cf2
lx7zq <thisforworkonly7@gmail.com>
2025-07-07 15:03:21 +0700

    ปรับปรุงการนำเข้าฟังก์ชันใน notification.router.js โดยเปลี่ยนจากการนำเข้าฟังก์ชันทั้งหมดใน notification.controller.js เพื่อให้โค้ดมีความชัดเจนและจัดการได้ง่ายขึ้น

--- routes/notification.router.js
+++ routes/notification.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const notificationController = require('../controllers/notification.controller');
+const { getAllNotifications, getLowStockNotifications, getExpiringNotifications, broadcastLineNotification, broadcastStockAlert } = require('../controllers/notification.controller');
 
 // ดึงการแจ้งเตือนทั้งหมด
 router.get('/all', notificationController.getAllNotifications);
